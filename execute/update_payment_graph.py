7f3e9e4d912aa7c32bb2a18295cea872d5e0c3a2
Ui <kyh571021@gmail.com>
2024-09-04 15:24:22 +0900

    위탁 운영업체 납부현황 그래프 그리기 기능추가

--- execute/update_payment_graph.py
+++ execute/update_payment_graph.py
@@ -6,66 +6,151 @@ from function import func_excel, func_pdf
 
 import re
 
+# 결의서 제목에서 월자 추출 제대로 되는지 확인하기
+# get_column_from_date, insert_value_to_merged_cell 전표일자 -> 월분으로 선택
+# 영우 구분 / 한국전자금융 구분
+# 씨앤코컴퍼니, 유니룩스, 스튜디오홍대 창고, 이경선, 윤영(예금), 박기철, 김정현, 허윤석, 김이중(호수 불일치), 송규만(호수 불일치)
+# 유니룩스, 숨코리아 빼고 다 없음
+# 미수금 반영?: 미수금 설정은 반영x
+# 여러 연도 (24.12 ~ 25.1) 일 경우 체크하기
+# 원장에 있지만 납부 현황에 없는 업체 처리 // 납부 현황에 있는 업체들만
+# 추후에 업체 추가시 납부현황에 원장 관리코드명으로 행 수기로 추가
+# 위탁료 관리비 분리: 적요 / 위탁, 시설사용, 임대
+# 납부현황, 원장 관리코드명 통일
+# 연구실 동호수
 
 # file : 그래프를 그리는 파일, data_set : 정보를 가져오는 파일
 def execute(file, data_set):
-    # data set에서 계정과목명이 대여료및사용료 인 것만 가져오기 (엑셀 파일 형식의 2차원배열)
-    data = func_excel.get_history_data_set_info(data_set)
+    # deposit_data: 계정과목명이 대여료및사용료 인 것만 중 미수금 설정 제외하고 가져오기 (엑셀 파일 형식의 2차원배열)
+    # received_data: 미수금 입금 데이터 가져오기
+    deposit_data, received_data = func_excel.get_history_data_set_info(data_set)
 
-    # 그래프의 검색어(=업체명) 모두 가져오기
-    keywords = func_excel.get_column_data(file, 'B')
-    filtered_keywords = [x for x in keywords if x is not None]
-    for keyword in filtered_keywords:
-        print(keyword)
+    # 그래프의 업체 정보 가져오기
+    file_data = func_excel.get_file_info(file)
 
-    # 그래프의 구분(D열) 모두 가져오기
-    partition = func_excel.get_column_data(file, 'D')
+    # 업체 명, 금액 리스트 표시
+    for keyword, money in file_data[:,0], file_data[:,3]:
+        print(keyword+': '+money)
 
-    # 그래프의 금액 모두 가져오기
-    money_values = func_excel.get_column_data(file, 'F')
+    # tenant 임차인
+    for idx, tenant in enumerate(file_data):
+        name = tenant[0]
+        money = tenant[3]
+        print(name + ' ' + tenant[1] + ' ' + tenant[2] + '(' + money + '원) >>>')
 
-    # data_set에서 뽑아온 결의 한 행 마다 루프 (resolution : 결의)
-    for resolution in data:
-        res_code = resolution[19]  # dataset의 관리코드명 - 그래프 파일의 검색어와 동일
-        res_money_value = resolution[8]  # dataset의 대변
-        res_subject_name = resolution[16]  # dataset의 계정과목명
-        res_title = resolution[4]  # dataset의 결의서 제목
+        for resolution in deposit_data:
+            res_code = resolution[19]  # dataset의 관리코드명 (T열) - 그래프 파일의 검색어와 동일
+            res_money_value = resolution[8]  # dataset의 대변 (I열)
+            res_title = resolution[4]  # dataset의 결의서 제목 (E열)
+            res_date = resolution[2]  # dataset의 전표일자 (C열)
+            if name == res_code and money == res_money_value:
+                row = idx
+                print("전표일자: ", res_date)
+                insert_result = format_date(res_date)
+                print("insert result : ", insert_result)  # insert_result : n.n입
+                insert_value_to_merged_cell(file, row, res_title, res_date, insert_result)
 
-        print("관리코드명 : ", res_code)
-        print("대변 : ", res_money_value)
-        print("계정과목명(대여료및사용료) : ", res_subject_name)
+        for resolution in received_data:
+            res_code = resolution[19]  # dataset의 관리코드명 (T열) - 그래프 파일의 검색어와 동일
+            res_money_value = resolution[7]  # dataset의 차변 (H열)
+            res_title = resolution[4]  # dataset의 결의서 제목 (E열)
+            res_date = resolution[2]  # dataset의 전표일자 (C열)
+            if name == res_code and money == res_money_value:
+                row = idx
+                print("**미수금 입금**")
+                print("전표일자: ", res_date)
+                insert_result = format_date(res_date)
+                print("insert result : ", insert_result)  # insert_result : n.n입
+                insert_value_to_merged_cell(file, row, res_title, res_date, insert_result)
 
-        row = None
-        for idx, keyword in enumerate(filtered_keywords):  # 검색어 루프
-            for money_value in money_values:  # 금액 루프
-                if keyword == res_code and money_value == res_money_value and keyword in res_title:  # 검색어가 data_set의 결의서 제목에 포함되고, 금액까지 같다면?
-                    row = idx
-                    print("전표일자(resolution[2]) : ", resolution[2])
-                    insert_result = format_date(resolution[2])  # resolution[2] : dataset의 전표일자
-                    print("insert result : ", insert_result)  # insert_result : n.n입
-                    insert_value_to_merged_cell(file, row, res_code, resolution[2], insert_result)
+
+
+# ############################################################################################################################
+#     # 그래프의 검색어(=업체명) 모두 가져오기
+#     keywords = func_excel.get_column_data(file, 'B')
+#     filtered_keywords = [x for x in keywords if x is not None]
+#     # 그래프의 구분(D열) 모두 가져오기
+#     category = func_excel.get_column_data(file, 'D')
+#
+#     # 그래프의 금액 모두 가져오기
+#     money_values = func_excel.get_column_data(file, 'E')
+#
+#     # data_set에서 뽑아온 결의 한 행 마다 루프 (resolution : 결의)
+#     for resolution in deposit_data:
+#         res_code = resolution[19]  # dataset의 관리코드명 (T열) - 그래프 파일의 검색어와 동일
+#         res_money_value = resolution[8]  # dataset의 대변 (I열)
+#         res_subject_name = resolution[16]  # dataset의 계정과목명 (Q열)
+#         res_title = resolution[4]  # dataset의 결의서 제목 (E열)
+#         res_date = resolution[2]    # dataset의 전표일자 (C열)
+#         res_brief = resolution[17]  # datset의 적요 (R열)
+#
+#         print("관리코드명 : ", res_code)
+#         print("대변 : ", res_money_value)
+#         print("계정과목명(대여료및사용료) : ", res_subject_name)
+#
+#         row = None
+#         for idx, keyword in enumerate(filtered_keywords):  # 검색어 루프
+#             for money_value in money_values:  # 금액 루프
+#                 if keyword == res_code and money_value == res_money_value and keyword in res_title:  # 검색어가 data_set의 결의서 제목에 포함되고, 금액까지 같다면?
+#                     row = idx
+#                     print("전표일자(resolution[2]) : ", res_date)
+#                     insert_result = format_date(res_date)
+#                     print("insert result : ", insert_result)  # insert_result : n.n입
+#                     insert_value_to_merged_cell(file, row, res_code, res_date, insert_result)
+#                 elif '교수' in res_brief and keyword[:3] in res_brief:
+#                     row = idx
+#                     print("전표일자(resolution[2]) : ", res_date)
+#                     insert_result = format_date(res_date)
+#                     print("insert result : ", insert_result)  # insert_result : n.n입
+#                     insert_value_to_merged_cell(file, row, res_code, res_date, insert_result)
+# ############################################################################################################################
 
 
 def extract_and_compute_difference(text):
     # 정규 표현식 패턴
     patterns = [
+        r'(\d{2}.\d{1,2}~\d{2}.\d{1,2}월분)' # yy.n~yy.m월분 형태
+        r'(\d{1,2}~\d{1,2}월분)',  # n~m월분 형태
         r'(\d{1,2}월분)',  # n월분 형태
-        r'(\d{1,2}~\d{1,2}월)'  # n~m월 형태
     ]
 
     numbers = []
 
-    for pattern in patterns:
-        matches = re.findall(pattern, text)
-        for match in matches:
+    # idx 로 케이스 나누는게 맞는지 체크
+    for idx, pattern in enumerate(patterns):
+        matches = re.findall(pattern, text) # 정규 표현식과 맞는 문자열 리스트로 반환
+        if matches:
+            # yy.n ~ yy.m, 숫자들 분리 후 2,4 번째 숫자만 추출
+            if idx==0:
+                annual_parts = re.findall(r'\d', matches[0])
+                numbers.extend(map(int, annual_parts))
+                del numbers[0]  # [1, 2, 3, 4] -> [2, 3, 4]
+                del numbers[1]  # [2, 3, 4] -> [2, 4]
+                numbers[1] = numbers[1]+13  # 월수 차이 계산 위해 끝나는 월에 +13
+                break
             # n~m월분 형태인 경우, 시작과 끝 숫자를 분리
-            if '~' in match:
-                range_parts = re.findall(r'\d+', match)
+            elif idx==1:
+                range_parts = re.findall(r'\d+', matches[0])
                 numbers.extend(map(int, range_parts))
-            # n월분 형태인 경우, 숫자만 추출
-            else:
-                month_parts = re.findall(r'\d+', match)
+                break
+            # # n월분 형태인 경우, 숫자만 추출
+            elif idx==2:
+                month_parts = re.findall(r'\d+', matches[0])
                 numbers.extend(map(int, month_parts))
+                break
+
+    if len(numbers) == 0:
+        print('결의서 제목의 날짜 양식이 맞지 않습니다.')
+
+        # if '~' in match:
+        #     range_parts = re.findall(r'\d+', match)
+        #     numbers.extend(map(int, range_parts))
+        # elif:
+        #     None
+        # # n월분 형태인 경우, 숫자만 추출
+        # else:
+        #     month_parts = re.findall(r'\d+', match)
+        #     numbers.extend(map(int, month_parts))
 
     # 숫자가 여러 개일 경우, 차이를 계산
     if len(numbers) > 1:
@@ -73,17 +158,16 @@ def extract_and_compute_difference(text):
     else:
         difference = 0  # 숫자가 하나거나 없으면 차이는 0으로 설정
 
-    return numbers, difference
+    return min(numbers), difference
 
-
-# insert_result: n.n입, resolution_date: dataset의 전표일자, text: dataset의 결의서제목
-def insert_value_to_merged_cell(filename, row, text, resolution_date, insert_result):
+# insert_result: n.n입, res_date: dataset의 전표일자, text: dataset의 결의서제목
+def insert_value_to_merged_cell(filename, row, text, res_date, insert_result):
     # 엑셀 파일 열기
     workbook = openpyxl.load_workbook(filename, read_only= False)
     sheet = workbook["서울캠"]  # 활성 시트 선택
 
     # extract_and_compute_difference 함수 호출
-    _, difference = extract_and_compute_difference(text)
+    date, difference = extract_and_compute_difference(text)
 
     # 차이에 따른 칸 수와 반환 값 결정
     if difference == 0:
@@ -105,7 +189,8 @@ def insert_value_to_merged_cell(filename, row, text, resolution_date, insert_res
     print("cells_to_merge", cells_to_merge)
 
     # 날짜에 맞는 열 찾기
-    date_str = resolution_date.strftime('%Y-%m-%d')
+    # date_str = res_date.strftime('%Y-%m-%d')
+    date_str = '2024-' + str(date)   # yyyy-mm
     print("date_str : ", date_str)
 
     column = func_excel.get_column_from_date(sheet, date_str)
--- function/func_excel.py
+++ function/func_excel.py
@@ -2,6 +2,7 @@ from openpyxl.styles import PatternFill
 from openpyxl import load_workbook
 import re
 
+# workbook.close() 추가 필요
 
 def check_line(sheet):
     max_row = sheet.max_row
@@ -70,27 +71,61 @@ def get_filtered_data(filename, sheet_name, target_column, filter_value):
             # 조건에 맞는 행을 필터링된 리스트에 추가
             filtered_rows.append([cell.value for cell in row])
 
+    workbook.close()
+
     return filtered_rows
 
 
+# 입금, 미수금 입금 데이터 가져오기
 def get_history_data_set_info(data_set):
     # 엑셀 파일 열기
     workbook = load_workbook(data_set)
     sheet = workbook.active  # 활성 시트 선택
 
-    # 데이터 저장을 위한 리스트
-    data = []
+    # 입금 데이터 저장을 위한 리스트
+    deposit_data = []
+    # 미수금 입금 데이터 저장을 위한 리스트
+    received_data = []
 
     # 모든 행을 반복하면서 A부터 AG열의 값을 가져오기
     for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=33):  # A부터 AG까지 33개의 열
         # Q 열의 값 확인 (Q 열은 17번째 열에 해당)
-        if row[16].value == '대여료및사용료':  # Q 열은 인덱스 16에 해당 - 대여료 및 사용료인 것들만 뽑아오기
+        if row[16].value == '대여료및사용료' and '미수금 설정' not in row[4].value:  # Q 열은 인덱스 16에 해당 - 대여료 및 사용료인 것들만 뽑아오기, 미수금 설정 건은 별도로 처리
             row_data = []
             for cell in row:
                 row_data.append(cell.value)
-            data.append(row_data)
+            deposit_data.append(row_data)
+        if '미수금 입금' in row[4].value and row[8].value == 0:
+            row_data = []
+            for cell in row:
+                row_data.append(cell.value)
+            received_data.append(row_data)
 
-    return data
+    workbook.close()
+
+    return deposit_data, received_data
+
+
+def get_file_info(file):
+    # 엑셀 파일 열기
+    workbook = load_workbook(file)
+    sheet = workbook.active  # 활성 시트 선택
+
+    # 업체 정보를 받기 위한 리스트
+    file_data = []
+
+    # 빈 행이 나올 때 까지 모든 행을 반복하면서 B부터 F열의 값을 가져오기
+    for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=2, max_col=6):  # B부터 F까지 5개의 열
+        if row[0].value == None:
+            break
+        row_data = []
+        for cell in row:
+            row_data.append(cell.value)
+        file_data.append(row_data)
+
+    workbook.close()
+
+    return file_data
 
 
 def get_column_data(filename, column):
@@ -106,20 +141,22 @@ def get_column_data(filename, column):
         cell_value = sheet[f'{column}{row}'].value
         data.append(cell_value)
 
+    workbook.close()
+
     return data
 
 
 def get_column_from_date(sheet, date_text):
     # 날짜 형식에서 연도와 월 추출
-    match = re.match(r'(\d{4})-(\d{2})-(\d{2})', date_text)
+    match = re.match(r'(\d{4})-(\d{2})', date_text)
     if not match:
-        raise ValueError("날짜 형식이 잘못되었습니다. 형식을 다음과 같이 맞춰주세요 : YYYY-MM-DD")
+        raise ValueError("날짜 형식이 잘못되었습니다. 형식을 다음과 같이 맞춰주세요 : YYYY-MM")
 
     year = match.group(1)[2:]  # 연도의 마지막 두 자리
     month = str(int(match.group(2))) + '월'  # 앞에 '0'을 제거한 월
 
-    # L열부터 탐색
-    start_column = 12  # L열의 인덱스는 12
+    # G열부터 탐색
+    start_column = 6  # G열의 인덱스는 6
     for col in range(start_column, sheet.max_column + 1):
         cell_value = sheet.cell(row=1, column=col).value  # 첫 행에서 찾기
         if cell_value and f"{year}.{month}" in cell_value:
